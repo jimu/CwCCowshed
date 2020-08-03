f9127fdc1e50d90fc71e5cd5b5ee9a75fad0a9a0
Jim Urbas <git@jimu.net>
2020-08-03 02:12:09 -0500

    clouds

---
+++ Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    float xRepeat = 1024 + 47;
+    [SerializeField] float speed = 10;
+
+    Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        float offset = (speed * Time.deltaTime - pos.x) % xRepeat;
+        pos.x = -offset;
+        transform.position = pos;
+    }
+}
